19f282f50bc4b1d289dc4eec84f71f86922c1057
Ndulue Stephanie <juststephofficial@gmail.com>
2021-11-17 13:09:25 +0100

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -6,21 +6,22 @@ function collect_ratings() {
     };
     let rating = 0;
 
+
+
+
+    const elements = document.querySelectorAll(".rating");
+    elements.forEach((element) => {
+        rating = parseInt(element.id.replace("star", ""));
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
+    });
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    };
     return ratings;
 };
-
 document.addEventListener('change', () => {
     const ratings = collect_ratings();
-    ratings.average = document.querySelector("#average").toFixed(2);
+    document.querySelector("#average").value = ratings.average.toFixed(2);
 });
-
-const elements = document.querySelectorAll(".rating");
-elements.forEach((element) => {
-    rating = parseInt(element.id.replace("star", ""));
-    ratings.count += parseInt(element.value);
-    ratings.sum += parseInt(element.value) * rating;
-});
-
-if (ratings.count !== 0) {
-    ratings.average = ratings.sum / ratings.count;
-};
