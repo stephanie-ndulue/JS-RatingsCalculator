41514121a248605f02d364ca1dd33a6fe0588fa1
Ndulue Stephanie <juststephofficial@gmail.com>
2021-11-17 12:49:21 +0100

    Completed module

--- js/ratings.js
+++ js/ratings.js
@@ -21,6 +21,6 @@ elements.forEach((element) => {
     ratings.sum += parseInt(element.value) * rating;
 });
 
-if (ratings.count != 0) {
+if (ratings.count !== 0) {
     ratings.average = ratings.sum / ratings.count;
 };
